277b0d8206ca96421ba1839629f35e42347adebb
laker007 <838205692@qq.com>
2017-08-23 18:04:17 +0800

    mp update

--- app.js
+++ app.js
@@ -6,26 +6,47 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-  getUserInfo:function(cb){
+  getUserInfo: function (cb) {
     var that = this
-    if(this.globalData.userInfo){
+    if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+    } else {
       //调用登录接口
       wx.login({
-        success: function () {
+        success: function (data) {
+          console.log(data.errMsg);
+          console.log(data.code);
+
           wx.getUserInfo({
             success: function (res) {
+              console.log(res);
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
               console.log(that.globalData);
+
+              wx.request({
+                url: 'http://localhost:3000/api/v1/login',
+                method: 'POST',
+                data: {
+                  js_code: data.code,
+                  encryptedData: res.encryptedData,
+                  iv: res.iv,
+                },
+                header: {
+                  'content-type': 'application/json'
+                },
+                success: function (res) {
+                  console.log(res)
+                }
+              })
+
             }
           })
         }
       })
     }
   },
-  globalData:{
-    userInfo:null
+  globalData: {
+    userInfo: null
   }
 })
