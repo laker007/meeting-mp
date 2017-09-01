b483e0b0262be94a022d88406530d286118ef9fa
laker007 <838205692@qq.com>
2017-09-01 18:07:09 +0800

    update

--- page/main/pages/temp/temp.js
+++ page/main/pages/temp/temp.js
@@ -2,16 +2,10 @@
 //获取应用实例
 var app = getApp()
 Page({
-    data: {
-
-    },
-
     onLoad: function () {
-        console.log('onLoad');
         app.getUserInfo(function (userInfo) {})
         wx.redirectTo({
             url: '../../index',
         })
-        console.log(app.globalData.register);
     },
 })
